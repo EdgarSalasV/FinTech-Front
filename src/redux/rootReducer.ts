19cb5490279f1acb1a3caefae6682b6a36662625
EdgarSalasV <edgardre01@gmail.com>
2020-06-25 21:15:56 -0500

    added redux arquitecture & tool libraries

---
+++ src/redux/rootReducer.ts
@@ -0,0 +1,18 @@
+import { all } from "redux-saga/effects";
+import { combineReducers } from "redux";
+
+import * as auth from "../app/modules/Auth/_redux/authRedux";
+// import { accountsSlice } from "./accounts/accountsSlice";
+// import { countriesSlice } from "./countries/countriesSlice";
+// import { currenciesSlice } from "./currencies/countriesSlice";
+
+export const rootReducer = combineReducers({
+  auth: auth.reducer,
+  // accounts: accountsSlice.reducer,
+  // countries: countriesSlice.reducer,
+  // PERRON: currenciesSlice.reducer, 
+});
+
+export function* rootSaga() {
+  yield all([auth.saga()]);
+}
